e7030858c35c748b6b294f41539648bb655ae382
lordjav <lord_javius@hotmail.com>
2024-05-17 18:14:22 -0500

    Adición de favicons, helpers y edición de layout

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, flash, redirect, render_template, request, session, sessions
+from werkzeug.security import generate_password_hash, check_password_hash
+from helpers import login_required
+
+
+app = Flask(__name__)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
