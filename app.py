889cac45dc654bc9e966518c204adf09de5e68d4
lordjav <lord_javius@hotmail.com>
2024-05-18 13:58:49 -0500

    Estilo de LogIn creado

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from helpers import login_required
 
 app = Flask(__name__)
 
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+
 @app.route("/login")
 def login():
     return render_template("login.html")
