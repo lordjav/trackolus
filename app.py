85fb7ef8e7597e1529446c92b4a8780cb62fe1f0
lordjav <lord_javius@hotmail.com>
2024-05-20 18:30:49 -0500

    Creación del archivo general_data.db, configuración de SQLAlchemy, bosquejo de layout.html, ruta para login en app.py.

--- app.py
+++ app.py
@@ -1,16 +1,54 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, flash, redirect, render_template, request, session, sessions
 from werkzeug.security import generate_password_hash, check_password_hash
 from helpers import login_required
 
-
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
 
 @app.route("/")
-@login_required
+#@login_required
 def index():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return redirect("/login")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return redirect("/login")
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return redirect("/login")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+    
