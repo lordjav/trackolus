0da5fbb80315b8aaf5d75c2d5dc813e9259d4a1c
lordjav <lord_javius@hotmail.com>
2024-05-27 12:40:56 -0500

    Small format changes completed

--- static/scripts.js
+++ static/scripts.js
@@ -1,31 +1,53 @@
-// :::MODAL SCRIPT:::
+// :::ADD PRODUCT MODAL:::
 // Get the modal
-let modal = document.getElementById("add-product");
+let addProductModal = document.getElementById("add-product");
 
 // Get the button that opens the modal
 let addButton = document.getElementById("add-button");
 
 // Get the elements that closes the modal
 let closeModalButton = document.getElementById("close-modal-button");
-let cancelModalButton = document.getElementById("cancel-modal-button");
+
+// When the user clicks the button, open the modal
+addButton.onclick = function() {
+  addProductModal.style.display = "block";
+}
+
+// When the user clicks on "x", close the modal
+closeModalButton.onclick = function() {
+  addProductModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    addProductModal.style.display = "none";
+  }
+}
+
+/*// :::VIEW PRODUCT MODAL:::
+// Get the modal
+let viewProductModal = document.getElementById("{{ product.image_route }}");
+
+/ Get the button that opens the modal
+let addButton = document.getElementById("add-button");
+
+// Get the elements that closes the modal
+let closeModalButton = document.getElementById("close-modal-button");
 
 // When the user clicks the button, open the modal
 addButton.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on "x" or "Cancel", close the modal
+// When the user clicks on "x", close the modal
 closeModalButton.onclick = function() {
   modal.style.display = "none";
 }
-
-cancelModalButton.onclick = function() {
-    modal.style.display = "none";
-  }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+}*/
