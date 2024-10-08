15e96cf4e16ed21e5d458604197465511767923b
lordjav <lord_javius@hotmail.com>
2024-10-08 17:00:59 -0500

    role permissions added to inventory

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import pdfkit, sqlalchemy, pandas, plotly.express, textwrap, csv, io, pytz, time, json
+import sqlalchemy, pandas, plotly.express, textwrap, csv, io, pytz, time, json
 from cs50 import SQL
 from flask import Flask, jsonify, redirect, render_template, session, send_file
 from flask import render_template_string, request, make_response, stream_with_context, Response
@@ -28,142 +28,6 @@ babel.init_app(
     )
 
 server.jinja_env.filters["cop"] = cop
-
-#Create inventory (list of products as a database) and 
-#catalogue (list of products as objects)
-#@server.context_processor
-@login_required
-def create_catalogue():
-    inventory = db.execute("""
-                           SELECT 
-                            i.id,
-                            i.SKU,
-                            i.product_name,
-                            i.buy_price,
-                            i.sell_price,
-                            i.author,
-                            i.addition_date,
-                            i.image_route,
-                            w.name AS warehouse,
-                            a.stock
-                           FROM inventory i
-                           JOIN allocation a ON i.id = a.product_id
-                           JOIN warehouses w ON a.warehouse = w.id
-                           """)
-
-    catalogue = {}
-    for element in inventory:
-        if element['id'] not in catalogue:
-            product = prototype_product(
-                element["id"], 
-                element["SKU"], 
-                element["product_name"], 
-                element["buy_price"], 
-                element["sell_price"], 
-                element["author"], 
-                element["addition_date"], 
-                element["image_route"],
-                )
-            catalogue[element['id']] = product
-        catalogue[element['id']].add_warehouse_stock(element['warehouse'], element['stock'])
-    
-    return list(catalogue.values())
-
-
-#Function: get data to create order
-def get_order_data():
-    products_list = request.form.getlist("products-selected")
-    name = request.form.get("customer-name")
-    id = request.form.get("customer-id")
-    phone = request.form.get("customer-phone")
-    email = request.form.get("customer-email")
-
-    customer_data = customer(name, id, phone, email)
-    products = []
-    total = 0
-    catalogue = create_catalogue()
-    
-    for element in catalogue:
-        if element.SKU in products_list:
-            products.append(element)
-    for item in products:
-        item.other_props['items_to_transact'] = int(request.form.get(item.SKU))
-        total += (item.other_props['items_to_transact'] * item.sell_price)
-
-    data = {'customer': customer_data, 'products': products, 'total': total}
-    return data
-
-
-#Function: configurate pdfkit and wkhtmltopdf to create pdf 
-def configurate_pdf(rendered):
-    path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)    
-    pdf = pdfkit.from_string(
-        rendered, 
-        False, 
-        options={"enable-local-file-access": ""}, 
-        configuration=config
-        )    
-    response = make_response(pdf)
-    response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
-
-    return response
-
-
-#Function: get type of movements ('inbound' or 'outbound') and convert 
-#database into a list of movement objects
-def separate_movements(type_of_movement):
-    movements = db.execute("""
-                           SELECT * 
-                           FROM movements m 
-                           JOIN products_movement p 
-                            ON m.id = p.movement_id 
-                           WHERE type = ? 
-                           ORDER BY date DESC
-                           """, type_of_movement)
-    movements_objects = []
-
-    for element in movements:
-        author_name = db.execute("""
-                                 SELECT name 
-                                 FROM users 
-                                 JOIN movements ON users.id = movements.author 
-                                 WHERE movements.author = ?
-                                 """, element["author"]
-                                 )[0]["name"]
-        counterpart = db.execute("""
-                                 SELECT name 
-                                 FROM customers_suppliers c
-                                 JOIN movements m 
-                                    ON c.id = m.counterpart 
-                                 WHERE m.counterpart = ?
-                                 """, element["counterpart"]
-                                 )
-        if not counterpart:
-            counterpart_name = ""
-        else:
-            counterpart_name = counterpart[0]["name"]
-        
-        order_in_list = False
-
-        for object in movements_objects:
-            if element["order_number"] == object.order_number:
-                order_in_list = True                
-                object.add_products(products_to_movements(element))
-                
-        if order_in_list == False:
-            movement = prototype_order(element["order_number"], 
-                                       element["type"], 
-                                       element["date"], 
-                                       author_name, 
-                                       counterpart_name
-                                       )            
-            movement.add_products(products_to_movements(element))
-            movements_objects.append(movement)
-
-    return movements_objects
-
 
 #Decorator: Makes the function 'get_locale' available directly in the template
 @server.context_processor
@@ -234,8 +98,13 @@ def logout():
 @role_required(['admin', 'user', 'observer'])
 def inventory():
     catalogue = create_catalogue()
+    
+    if session['role'] == 'observer':
+        template = 'inventory-o.html'
+    else:
+        template = 'inventory.html'
 
-    return render_template("inventory.html", catalogue=catalogue)
+    return render_template(template, catalogue=catalogue)
 
 
 @server.route("/ordered_inventory/<parameter>")
@@ -290,6 +159,7 @@ def ordered_inventory(parameter):
 
 @server.route("/add_product", methods=["POST"])
 @login_required
+@role_required(['admin', 'user'])
 def add_product():
     try:
         #Ensure product name is submitted
@@ -371,7 +241,7 @@ def add_product():
 
 @server.route("/purchase_order", methods=["GET", "POST"])
 @login_required
-@role_required(['admin'])
+@role_required(['admin', 'user'])
 def purchase_order():
     if request.method == "POST":
         data = get_order_data()
--- trackolus/helpers.py
+++ trackolus/helpers.py
@@ -1,7 +1,9 @@
+import pdfkit
 from cs50 import SQL
 from datetime import datetime
-from flask import redirect, session, request, g, render_template
+from flask import redirect, session, request, g, render_template, make_response
 from functools import wraps
+from trackolus.classes import *
 
 db = SQL("sqlite:///general_data.db")
 
@@ -82,3 +84,139 @@ def products_to_movements(element):
     product["price"] = element["price"]
 
     return product
+
+
+#Create inventory (list of products as a database) and 
+#catalogue (list of products as objects)
+@login_required
+def create_catalogue():
+    inventory = db.execute("""
+                           SELECT 
+                            i.id,
+                            i.SKU,
+                            i.product_name,
+                            i.buy_price,
+                            i.sell_price,
+                            i.author,
+                            i.addition_date,
+                            i.image_route,
+                            w.name AS warehouse,
+                            a.stock
+                           FROM inventory i
+                           JOIN allocation a ON i.id = a.product_id
+                           JOIN warehouses w ON a.warehouse = w.id
+                           """)
+
+    catalogue = {}
+    for element in inventory:
+        if element['id'] not in catalogue:
+            product = prototype_product(
+                element["id"], 
+                element["SKU"], 
+                element["product_name"], 
+                element["buy_price"], 
+                element["sell_price"], 
+                element["author"], 
+                element["addition_date"], 
+                element["image_route"],
+                )
+            catalogue[element['id']] = product
+        catalogue[element['id']].add_warehouse_stock(element['warehouse'], element['stock'])
+    
+    return list(catalogue.values())
+
+
+#Function: get data to create order
+def get_order_data():
+    products_list = request.form.getlist("products-selected")
+    name = request.form.get("customer-name")
+    id = request.form.get("customer-id")
+    phone = request.form.get("customer-phone")
+    email = request.form.get("customer-email")
+
+    customer_data = customer(name, id, phone, email)
+    products = []
+    total = 0
+    catalogue = create_catalogue()
+    
+    for element in catalogue:
+        if element.SKU in products_list:
+            products.append(element)
+    for item in products:
+        item.other_props['items_to_transact'] = int(request.form.get(item.SKU))
+        total += (item.other_props['items_to_transact'] * item.sell_price)
+
+    data = {'customer': customer_data, 'products': products, 'total': total}
+    return data
+
+
+#Function: configurate pdfkit and wkhtmltopdf to create pdf 
+def configurate_pdf(rendered):
+    path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)    
+    pdf = pdfkit.from_string(
+        rendered, 
+        False, 
+        options={"enable-local-file-access": ""}, 
+        configuration=config
+        )    
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+
+    return response
+
+
+#Function: get type of movements ('inbound' or 'outbound') and convert 
+#database into a list of movement objects
+def separate_movements(type_of_movement):
+    movements = db.execute("""
+                           SELECT * 
+                           FROM movements m 
+                           JOIN products_movement p 
+                            ON m.id = p.movement_id 
+                           WHERE type = ? 
+                           ORDER BY date DESC
+                           """, type_of_movement)
+    movements_objects = []
+
+    for element in movements:
+        author_name = db.execute("""
+                                 SELECT name 
+                                 FROM users 
+                                 JOIN movements ON users.id = movements.author 
+                                 WHERE movements.author = ?
+                                 """, element["author"]
+                                 )[0]["name"]
+        counterpart = db.execute("""
+                                 SELECT name 
+                                 FROM customers_suppliers c
+                                 JOIN movements m 
+                                    ON c.id = m.counterpart 
+                                 WHERE m.counterpart = ?
+                                 """, element["counterpart"]
+                                 )
+        if not counterpart:
+            counterpart_name = ""
+        else:
+            counterpart_name = counterpart[0]["name"]
+        
+        order_in_list = False
+
+        for object in movements_objects:
+            if element["order_number"] == object.order_number:
+                order_in_list = True                
+                object.add_products(products_to_movements(element))
+                
+        if order_in_list == False:
+            movement = prototype_order(element["order_number"], 
+                                       element["type"], 
+                                       element["date"], 
+                                       author_name, 
+                                       counterpart_name
+                                       )            
+            movement.add_products(products_to_movements(element))
+            movements_objects.append(movement)
+
+    return movements_objects
+
